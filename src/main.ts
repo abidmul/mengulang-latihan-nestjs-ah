a12ea115a08bf1121d7c1e3adc33c57f045f8b42
abidmul <mulawaabid@gmail.com>
2024-08-13 18:37:53 +0700

    View dalam NestJS-1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { resolve } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(resolve('./public'));
+  app.setBaseViewsDir(resolve('./views'));
+  app.setViewEngine('./views');
+
   await app.listen(3000);
 }
 bootstrap();
